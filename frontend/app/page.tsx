5955505db75fadd438813f122ac43faf3c303d69
Anand8198 <h9.amankumar@outlook.com>
2025-11-27 15:22:40 +0530

    new:initial commit

---
+++ frontend/app/page.tsx
@@ -0,0 +1,77 @@
+"use client";
+import { useState } from "react";
+
+export default function Home() {
+  const [fields, setFields] = useState([{ value: "" }]);
+
+  const addField = () => {
+    setFields([...fields, { value: "" }]);
+  };
+
+  const handleChange = (index: number, e: any) => {
+    const newFields = [...fields];
+    newFields[index].value = e.target.value;
+    setFields(newFields);
+  };
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    console.log("Form Data:", fields);
+  };
+
+  return (
+    <div className="min-h-screen w-full bg-gray-100 flex justify-center items-start py-10">
+      <div className="flex gap-8">
+        <div className="flex flex-col space-y-6 w-full max-w-xl">
+          <form
+            onSubmit={handleSubmit}
+            className="p-6 bg-white shadow-lg rounded-lg w-full space-y-4"
+          >
+            <input
+              type="text"
+              className="border p-3 rounded w-full text-2xl text-black focus:outline-blue-500"
+              placeholder="Untitled Form"
+            />
+
+            <input
+              type="text"
+              className="border p-3 rounded w-full text-sm text-black"
+              placeholder="Form Description"
+            />
+          </form>
+
+          {/* Dynamic Question List */}
+          <div className="p-6 bg-white shadow-lg rounded-lg w-full space-y-3">
+            <div className="flex flex-col space-y-3 rounded">
+              {fields.map((field, index) => (
+                <input
+                  key={index}
+                  type="text"
+                  value={field.value}
+                  onChange={(e) => handleChange(index, e)}
+                  className="border p-3 rounded w-full"
+                  placeholder="Untitled Question"
+                />
+              ))}
+            </div>
+            <button
+              type="submit"
+              className="px-4 py-2 bg-green-600 text-white rounded"
+            >
+              Submit
+            </button>
+          </div>
+        </div>
+        <div className="flex flex-col bg-white shadow-lg rounded-lg p-1 h-fit">
+          <button
+            type="button"
+            onClick={addField}
+            className="px-2 text-gray-500 text-2xl rounded-3xl border border-gray-300"
+          >
+            +
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
